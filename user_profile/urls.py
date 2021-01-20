81ceb79a21017d5165294619f756ded60b1e4b95
Denis Tamkovich <tamkovichdenis@gmail.com>
2021-01-20 21:59:31 +0300

    Added delete profile

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,4 +5,5 @@ from user_profile import views
 urlpatterns = [
     path('<str:username>/', views.profile, name='user-profile'),
     path('<str:username>/edit/', views.edit_profile, name='edit-profile'),
+    path('<str:username>/delete/', views.delete_profile, name='delete-profile')
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
+from user_profile.models import Profile
+
 
 @login_required
 def profile(request, username):
@@ -10,5 +12,25 @@ def profile(request, username):
 
 
 def edit_profile(request, username):
+    print(request.POST)
     user = get_object_or_404(User, username=username)
+    if request.method == 'POST':
+        user.username = request.POST.get('username')
+        profile, _ = Profile.objects.get_or_create(user=user)
+        profile.phone = request.POST.get('phone')
+        profile.country = request.POST.get('country')
+        user.save()
+        profile.save()
     return render(request, "user/edit_profile.html", {"user": user})
+
+
+def delete_profile(request, username):
+    """
+    Фукнция удаляет пользователя при запросе POST,
+    и возвращает кнокпу на удаление пользователя при GET
+    """
+    user = get_object_or_404(User, username=username)
+    if request.method == 'POST':
+        user.delete()
+        return render(request, "user/successfully_delete_profile.html")
+    return render(request, "user/delete_profile.html", {"user": user})
