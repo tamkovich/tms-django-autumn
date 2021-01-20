d46b672cc3f526be491e05112597962dfcef525e
Denis Tamkovich <tamkovichdenis@gmail.com>
2021-01-20 20:45:43 +0300

    Add user edit

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,4 +4,5 @@ from user_profile import views
 
 urlpatterns = [
     path('<str:username>/', views.profile, name='user-profile'),
+    path('<str:username>/edit/', views.edit_profile, name='edit-profile'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
+@login_required
 def profile(request, username):
-    user = User.objects.get(username=username)
+    user = get_object_or_404(User, username=username)
     return render(request, "user/profile.html", {"user": user})
+
+
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, "user/edit_profile.html", {"user": user})
